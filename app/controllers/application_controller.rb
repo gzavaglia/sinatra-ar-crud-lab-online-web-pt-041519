d76dea000f813bb492f9ba9cc5f021eea5dee6fd
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-07-31 18:50:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,9 @@ class ApplicationController < Sinatra::Base
     redirect to "/articles/#{@article.id}"
   end
   #delete
-  get 'models/:id/delete' do 
-    erb :delete
+  delete '/articles/:id/delete' do #delete action
+    @article = Article.find_by_id(params[:id])
+    @article.delete
+    redirect to '/articles'
   end
 end
