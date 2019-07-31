1f916794c2926e4996f751789ce4612aaa57ca1a
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-07-31 18:44:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
   
   post '/articles' do
     @article = Article.create(params)
-    redirect '/articles'
+    redirect to "/articles/#{@article.id}"'
   end
   
   #read
